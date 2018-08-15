82e23af25332c63e4fae1b8f7df5d86943cdd042
TJ <philosophy.cheer@gmail.com>
2018-08-15 09:35:03 -0400

    First upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    alert("The AI chose paper. You lose!");
+}
+
+function myFunction1() {
+    alert("The AI chose scissors. You lose!");
+}
+
+function myFunction2() {
+    alert("The AI chose rock. You lose!");
+}
+
+function myFunction3() {
+    alert("A strange game. The only winning move is not to play.");
+}
